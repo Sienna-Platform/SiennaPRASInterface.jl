ac89a5069d8d1b076b318b4d4a5a68b199dcd72e
Dhulipala <sdhulipa@nrel.gov>
2025-12-22 14:08:06 -0700

    Minor changes to Hydro_Planning

--- ext/Hydro_Planning.jl
+++ ext/Hydro_Planning.jl
@@ -8,6 +8,8 @@ using HydroPowerSimulations  # This is now available because the extension loade
 # Overload or extend a function from your main package
 function SiennaPRASInterface.run_hydro_planning(sys::PSY.System)
     println("Using HydroPowerSimulations.jl to generate energy/water budgets...")
+    # Call HPS here
+    # Add time series to HydroReservoir
     return nothing
 end
 
